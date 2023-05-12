80adbc44de47e4e60f0e59a229ade7a625f37849
paddy-pyker <paddypyker@gmail.com>
2023-05-12 09:01:26 -0400

    base application configured

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const port = 3000
 
+app.use(cors());
+
 app.get('/',(req,res) => {
-	res.send('Hello World!')
+	res.json({
+		status:'Hello World!'
+	})
 })
 
 app.listen(port,() => {
